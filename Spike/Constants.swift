c103ec36826909bd5d389593a9c68fcdd92ff5bb
Marta Tarragó Morán <martatarrago@gmail.com>
2016-02-21 06:38:14 +0100

    Completed part 7

--- Spike/Constants.swift
+++ Spike/Constants.swift
@@ -15,9 +15,14 @@ import Foundation
 
 
 // MARK: Global Constants
-var kJSON_URL: NSString! = "https://itunes.apple.com/us/rss/topmusicvideos/limit=10/json"
+let kJSON_URL: NSString = "https://itunes.apple.com/us/rss/topmusicvideos/limit=10/json"
 
 // MARK: Type Alias
 typealias JSONDictionary = [String: AnyObject]
 typealias JSONArray = Array<AnyObject>
 
+
+// MARK: Reachability Constants
+let WIFI = "WIFI Available"
+let NOACCESS = "No Internet Access"
+let WWAN = "Cellular Access Available"
